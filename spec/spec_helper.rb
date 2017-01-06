0931f93d28550194ec1e1bd6df381a9d5b3b2d9d
tahirstamboli <tahir.tamboli@reancloud.com>
2017-01-06 18:13:55 +0530

    Remote host testing using serverspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,37 @@
+require 'serverspec'
+require 'net/ssh'
+
+set :backend, :ssh
+
+if ENV['ASK_SUDO_PASSWORD']
+  begin
+    require 'highline/import'
+  rescue LoadError
+    fail "highline is not available. Try installing it."
+  end
+  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
+else
+  set :sudo_password, "password"
+end
+
+
+#host = ENV['TARGET_HOST']
+#host = "104.131.191.140"
+options = Net::SSH::Config.for(host)
+options[:user] = "user"
+options[:host_name] = "192.168.xx.xxx"
+options[:password] = "password"
+
+set :host,        options[:host_name] || host
+set :ssh_options, options
+
+# Disable sudo
+# set :disable_sudo, true
+
+
+# Set environment variables
+# set :env, :LANG => 'C', :LC_MESSAGES => 'C'
+
+# Set PATH
+# set :path, '/sbin:/usr/local/sbin:$PATH'
+
