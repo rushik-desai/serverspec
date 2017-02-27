42df63a089eebe65a5230559a5c8b325b5165c05
tahirstamboli <tahir.tamboli@reancloud.com>
2017-02-27 16:45:56 +0530

    Read ssh properties from environment variables.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,16 +11,25 @@ if ENV['ASK_SUDO_PASSWORD']
   end
   set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
 else
-  set :sudo_password, "password"
+  set :sudo_password, ENV['SPEC_PASSWORD']
 end
 
 
 #host = ENV['TARGET_HOST']
 #host = "104.131.191.140"
+
 options = Net::SSH::Config.for(host)
-options[:user] = "user"
-options[:host_name] = "192.168.xx.xxx"
-options[:password] = "password"
+options[:user] = ENV['SPEC_USER']
+options[:host_name] = ENV['SPEC_HOST_NAME']
+
+if ENV['SPEC_HOST_AUTHENTICATION']== "key"
+  key = []
+  key << File.read("key/#{ENV['SPEC_USER']}_#{ENV['SPEC_IP']}.key")
+  options[:keys_only] = TRUE
+  options[:key_data] = key
+elsif ENV['SPEC_HOST_AUTHENTICATION'] == "password"
+  options[:password] = ENV['SPEC_PASSWORD']
+end
 
 set :host,        options[:host_name] || host
 set :ssh_options, options
