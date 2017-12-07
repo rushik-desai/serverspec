50b628960b6e24389428eb2f2206bdc7a6c79345
rushik-desai <30500558+rushik-desai@users.noreply.github.com>
2017-12-07 17:16:50 -0500

    Update sample_spec.rb

--- spec/example/sample_spec.rb
+++ spec/example/sample_spec.rb
@@ -4,12 +4,3 @@ require 'spec_helper'
 describe package('httpd') do
   it { should be_installed }
 end
-
-# check for 8080 port listening
-describe port('8080') do
-  it { should be_listening }
-end
-
-describe command('sudo service httpd status') do
-  its(:exit_status) { should eq 0}
-end
