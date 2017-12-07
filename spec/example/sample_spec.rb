370c5afa5d3f58af94a4fcbf4015934de4c8f238
rushik-desai <30500558+rushik-desai@users.noreply.github.com>
2017-12-07 14:02:08 -0500

    test script

--- spec/example/sample_spec.rb
+++ spec/example/sample_spec.rb
@@ -1,21 +1,82 @@
 require 'spec_helper'
 
-describe package('mysql-client-5.5') do
+# check for zip package
+describe package('zip') do
   it { should be_installed }
 end
 
-describe service('mysql') do
-  it { should be_running }
-end
-
-describe package('maven3') do
+# check for httpd package
+describe package('httpd') do
   it { should be_installed }
 end
 
-describe service('cron') do
-  it { should be_running }
+# check for 8080 port listening
+describe port('8080') do
+  it { should be_listening }
 end
 
-describe file('/etc/passwd') do
-  it { should be_file }
+# check if java 1.8.0 installed
+describe package('java-1.8.0-openjdk') do
+  it { should be_installed }
+end
+
+# check for aha-portal directory
+describe file('/opt/aha-portal') do
+  it { should be_directory }
+end
+
+# check for aha directory
+describe file('/var/log/aha') do
+  it { should be_directory }
+end
+
+# check for aha-pmp-portal.war file
+describe file('/opt/aha-portal/aha-pmp-portal.war') do
+  it { should exist }
+end
+
+#check for aha-portal user
+describe user('aha-portal') do
+  it { should exist }
+end
+
+# check for aha-portal-httpd.conf
+describe file('/etc/httpd/conf.d/aha-portal-httpd.conf') do
+  it { should exist }
+end
+
+# check for aha-portal service file
+describe file('/etc/init.d/aha-portal') do
+  it { should exist }
+end
+
+# check for awslogs package
+describe package('awslogs') do
+  it { should be_installed }
+end
+
+# check for awslogs.conf
+describe file('/etc/awslogs/awslogs.conf') do
+  it { should exist }
+end
+
+# check if awslogs service is enabled and running
+describe service('awslogs') do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+# check for httpd service running and enabled
+describe service('httpd') do
+  it { should be_enabled }
+end
+
+describe command('sudo service httpd status') do
+  its(:exit_status) { should eq 0}
+end
+
+# check if aha-portal service is enabled and running
+describe service('aha-portal') do
+  it { should be_running }
+  it { should be_enabled }
 end
