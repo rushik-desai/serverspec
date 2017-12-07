a7224db5c255c536e91a8ef5db0b1c078391f3c7
rushik-desai <30500558+rushik-desai@users.noreply.github.com>
2017-12-07 17:04:40 -0500

    Update sample_spec.rb

--- spec/example/sample_spec.rb
+++ spec/example/sample_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
-# check for zip package
-describe package('zip') do
-  it { should be_installed }
-end
-
 # check for httpd package
 describe package('httpd') do
   it { should be_installed }
@@ -13,16 +8,6 @@ end
 # check for 8080 port listening
 describe port('8080') do
   it { should be_listening }
-end
-
-# check for awslogs package
-describe package('awslogs') do
-  it { should be_installed }
-end
-
-# check for httpd service running and enabled
-describe service('httpd') do
-  it { should be_enabled }
 end
 
 describe command('sudo service httpd status') do
